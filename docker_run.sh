e0bbfa49cf6a88351ad98e42a504042ad3082b67
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-07-23 16:26:41 +0900

    mount data/

--- docker_run.sh
+++ docker_run.sh
@@ -3,6 +3,7 @@ export GIT_ROOT=$(cd $(dirname $0)/.. ; pwd)
 sudo docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY \
 	--device /dev/bus/usb \
 	--device /dev/video0:/dev/video0:mwr \
+	-v $GIT_ROOT/cleargrasp-docker/data:/root/cleargrasp/data \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix \
 	cleargrasp:100
  
